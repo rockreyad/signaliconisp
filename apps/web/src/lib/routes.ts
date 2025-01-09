07b175956525b67d2b32b4d005f34c1e563cbc4f
rockreyad <rockreyad@gmail.com>
2025-01-09 15:29:06 +0600

    chore: remove all files

--- apps/web/src/lib/routes.ts
+++
@@ -1,5 +0,0 @@
-export const PUBLIC_ROUTES = ["/", "/signin", "/signup"];
-export const PRIVATE_ROUTES = ["/dashboard"];
-export const DEFAULT_REDIRECT = "/dashboard";
-export const LOGIN_REDIRECT = "/signin";
-export const ROOT = "/";
