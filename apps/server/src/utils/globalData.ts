d8e96fc948ceaadbff4479d3cb971e9df6d64a86
rockreyad <rockreyad@gmail.com>
2025-01-28 11:10:22 +0600

    chore: empty

--- apps/server/src/utils/globalData.ts
+++
@@ -1,7 +0,0 @@
-let idToken: string | null = null;
-
-export const setGlobalIdToken = (info: string): void => {
-  idToken = info;
-};
-
-export const getGlobalIdToken = (): string | null => idToken;
--- apps/web/jsvectormap.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "jsvectormap" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const jsVectorMap: any;
-  export default jsVectorMap;
-}
