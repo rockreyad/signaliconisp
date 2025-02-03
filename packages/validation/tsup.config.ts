59abd952a919aaa687cb9b17ffe1f05e5f8828ba
rockreyad <rockreyad@gmail.com>
2025-02-04 00:24:34 +0600

    chore: prisma package build

--- packages/validation/tsup.config.ts
+++ packages/validation/tsup.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/*.ts"],
+  entry: [
+    "src/index.ts",
+    "src/auth.ts",
+    "src/common.ts",
+    "src/user.ts",
+    "src/api.ts",
+    "src/package.ts",
+  ],
   format: ["esm"],
   dts: true,
   clean: true,
   splitting: false,
   treeshake: true,
+  outDir: "dist",
 });
